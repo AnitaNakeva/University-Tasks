d8753e1611289d3502030060398784e399025dfd
AnitaNakeva <anita_nakevaa@abv.bg>
2024-12-18 18:08:59 +0200

    feat: created windows form application for drawing forms

---
+++ Form1_homework/Form1_homework/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Form1_homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.btnFigure = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.Location = new System.Drawing.Point(132, 402);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(121, 34);
+            this.btnMedia.TabIndex = 0;
+            this.btnMedia.Text = "Media";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
+            // 
+            // btnFigure
+            // 
+            this.btnFigure.Location = new System.Drawing.Point(332, 402);
+            this.btnFigure.Name = "btnFigure";
+            this.btnFigure.Size = new System.Drawing.Size(121, 34);
+            this.btnFigure.TabIndex = 1;
+            this.btnFigure.Text = "Figure";
+            this.btnFigure.UseVisualStyleBackColor = true;
+            this.btnFigure.Click += new System.EventHandler(this.btnFigure_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(721, 402);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(121, 34);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(721, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 112);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 534);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnFigure);
+            this.Controls.Add(this.btnMedia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMedia;
+        private System.Windows.Forms.Button btnFigure;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1_homework/Form1_homework/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace Form1_homework
+{
+    //Диян Калайджиев F108356
+
+    public partial class Form1 : Form
+    {
+        Graphics gr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gr = CreateGraphics();
+            gr.SmoothingMode = SmoothingMode.HighQuality;
+        }
+
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("animatedCircle.gif");
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+
+            gr.Clear(this.BackColor);
+        }
+
+        private void btnFigure_Click(object sender, EventArgs e)
+        {
+            Pen redPen = new Pen(Color.Red, 10);
+            Pen bluePen = new Pen(Color.Blue, 10);
+
+            int centerX = 200;
+            int baseY = 100;
+            int arcWidthStep = 35;
+            int arcHeight = 200;
+            int numArcs = 5;
+
+            for (int i = 0; i < numArcs; i++)
+            {
+                int currentWidth = arcWidthStep * (i + 1);
+
+                int currentY = baseY - (i * 10);
+
+                Rectangle rect = new Rectangle(
+                    centerX - currentWidth / 2,
+                    currentY,
+                    currentWidth, arcHeight
+                );
+
+                if (i % 2 == 0)
+                {
+                    gr.DrawArc(bluePen, rect, 180, 180);
+                }
+                else
+                {
+                    gr.DrawArc(redPen, rect, 180, 180);
+                }
+            }
+        }
+    }
+}
